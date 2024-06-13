c88b54ad34c06eac5edae352b07a7d818a9ea9dd
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-06-13 23:33:46 +0300

    fix bugs in t1 and 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -9,7 +9,7 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	DECLARE avg_score FLOAT;
-	SET avg_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
+	SET avg_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
 	UPDATE users SET average_score = avg_score WHERE id = user_id;
 END$$
 DELIMITER ;
