73bec2a26930a292b637b09cdcfe6149bac4872e
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-06-19 23:19:55 +0300

    braces (wasnt wearing glasses i guess)

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """Implementing an expiring web cache and tracker
 """
-import requests
+from functools import wraps
 import redis
+import requests
 from typing import Callable
 
 
 def page_counter(func: Callable) -> Callable:
     """Count the number of times a url has been visited.
     """
+    @wraps(func)
     def wrapper(*args, **kwargs):
         """Wrapper function for page_counter.
             Sets the number of times a url has been visited in a redis cache.
@@ -17,8 +19,8 @@ def page_counter(func: Callable) -> Callable:
         cache = redis.Redis()
         url = args[0]
         with cache.pipeline() as pipe:
-            pipe.incr(f"count:({url})")
-            pipe.expire(f"count:({url})", 10)
+            pipe.incr(f"count:{url}")
+            pipe.expire(f"count:{url}", 10)
             pipe.execute()
         return func(*args, **kwargs)
     return wrapper
@@ -28,7 +30,7 @@ def get_count(url: str) -> int:
     """Get the number of times a URL has been visited.
     """
     cache = redis.Redis()
-    return int(cache.get(f"count:({url})") or 0)
+    return int(cache.get(f"count:{url}") or 0)
 
 
 @page_counter
@@ -45,6 +47,7 @@ if __name__ == "__main__":
            + "5000/url/https://www.google.com")
     for _ in range(5):
         get_page(url)
+    print(get_page(url))
     print(get_count(url))
     sleep(10)
     print(get_count(url))
